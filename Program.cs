d1886501b28a6a3f0fb93b9da4806b8929b9504a
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-04-23 23:11:39 -0300

    59. Interacting with the terminal

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿InitialOperation();
+
+
+void InitialOperation()
+{
+    while (true)
+    {
+        Console.WriteLine("Enter the desired operation");
+        Console.WriteLine("1 - Create List");
+        Console.WriteLine("2 - Edit List");
+        var desiredOperation = Console.ReadLine();
+        if (desiredOperation == "1")
+            OperationCreateList();
+        else if(desiredOperation == "2")
+        {
+            OperationEditList();
+        }
+    }
+}
+
+
+void OperationCreateList()
+{
+    Console.WriteLine("Name: ");
+    var name = Console.ReadLine();
+    Console.WriteLine("Date: ");
+    var date = Console.ReadLine();
+    InitialOperation();
+}
+
+
+void OperationEditList()
+{
+    while (true)
+    {
+        Console.WriteLine("Enter the desired operation");
+        Console.WriteLine("1 - Add Items");
+        Console.WriteLine("2 - Edit Items");
+        var desiredOperation = Console.ReadLine();
+        InitialOperation();
+    }
+}
