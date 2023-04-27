39314cb53facba16aeaaa340c0ec46e5b14e3dc3
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-04-26 22:42:34 -0300

    61. Create List via terminal

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿InitialOperation();
+﻿using Model;
+
+InitialOperation();
 
 void InitialOperation()
 {
@@ -21,8 +23,11 @@ void OperationCreateList()
 {
     Console.WriteLine("Name: ");
     var name = Console.ReadLine();
-    Console.WriteLine("Date: ");
-    var date = Console.ReadLine();
+    Console.WriteLine("Desired date: ");
+    var desiredDate = Console.ReadLine();
+    var convertedDesiredDate = DateTime.Parse(desiredDate);
+    var shoppingList = new ShoppingList(name, convertedDesiredDate);
+    Console.WriteLine($"{shoppingList.Name} list created for {shoppingList.DesiredDateOfPurchase} date.");
     InitialOperation();
 }
 
--- model/list.cs
+++ model/list.cs
@@ -1,11 +1,11 @@
 namespace Model;
 
-public class List
+public class ShoppingList
 {
     public readonly string Name;
     public DateTime DesiredDateOfPurchase { get; private set; }
 
-    public List(string name, DateTime desiredDateOfPurchase)
+    public ShoppingList(string name, DateTime desiredDateOfPurchase)
     {
         Name = name;
         DesiredDateOfPurchase = desiredDateOfPurchase;
