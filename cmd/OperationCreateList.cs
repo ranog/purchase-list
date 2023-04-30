a477da2f56b4bb17369a8a18d51c8b05a187eca2
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-04-30 14:45:52 -0300

    69. Anemic validation

--- cmd/OperationCreateList.cs
+++ cmd/OperationCreateList.cs
@@ -7,9 +7,16 @@ public static class OperationCreateList{
     public static void Execute(){
         Console.WriteLine("Name: ");
         var name = Console.ReadLine();
+        if(string.IsNullOrEmpty(name)){
+            Console.WriteLine("Invalid name.");
+            InitialOperation.Execute();
+        }
         Console.WriteLine("Desired date: ");
         var desiredDate = Console.ReadLine();
-        var convertedDesiredDate = DateTime.Parse(desiredDate);
+        if(!DateTime.TryParse(desiredDate, out var convertedDesiredDate)){
+            Console.WriteLine("Invalid date.");
+            InitialOperation.Execute();
+        }
         var shoppingList = new ShoppingList(name, convertedDesiredDate);
         RepositoryList.Add(shoppingList);
         Console.WriteLine($"{shoppingList.Name} list created for {shoppingList.DesiredDateOfPurchase} date.");
