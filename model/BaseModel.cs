196fcdcfe7b6b83796eb2faf4fc90f278baf59b5
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-06-23 01:13:22 -0300

    fix

--- model/BaseModel.cs
+++ model/BaseModel.cs
@@ -4,7 +4,7 @@ public abstract class BaseModel{
 
     public List<string> Errors { get; set; } = new List<string>();
 
-    public void AddValidation(bool isInvalid, string message){
+    public void AddValidation(bool isInvalid, string message) {
         
         if(isInvalid)
             Errors.Add(message);
